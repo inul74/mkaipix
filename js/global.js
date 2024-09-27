673672ad1b84c3c10dac7f6f6609524c184f77b9
inul74 <zmusthofa74@gmail.com>
2024-09-28 00:22:30 +0700

    ripple effect

--- js/global.js
+++ js/global.js
@@ -6,6 +6,12 @@
 "use strict";
 
 /**
+ * Imports
+ */
+
+import { ripple } from "./utils/ripple.js";
+
+/**
  * Header on-scroll state
  */
 
@@ -14,3 +20,12 @@ const /** {NodeElement} */ $header = document.querySelector("[data-header]");
 window.addEventListener("scroll", () => {
   $header.classList[window.scrollY > 50 ? "add" : "remove"]("active");
 });
+
+/**
+ * Add ripple effect
+ */
+
+const /** {NodeList} */ $rippleElems =
+    document.querySelectorAll("[data-ripple]");
+
+$rippleElems.forEach(($rippleElem) => ripple($rippleElem));
