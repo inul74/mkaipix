4dfc1cd85eab041507eb0a38a30e0017820900e0
inul74 <zmusthofa74@gmail.com>
2024-09-28 21:32:16 +0700

    navigation drawer structure & style

--- js/global.js
+++ js/global.js
@@ -10,6 +10,7 @@
  */
 
 import { ripple } from "./utils/ripple.js";
+import { addEventOnElements } from "./utils/event.js";
 
 /**
  * Header on-scroll state
@@ -29,6 +30,21 @@ const /** {NodeList} */ $rippleElems =
     document.querySelectorAll("[data-ripple]");
 
 $rippleElems.forEach(($rippleElem) => ripple($rippleElem));
+
+/**
+ * Navbar toggle for mobile screen
+ */
+
+const /** {NodeList} */ $navTogglers =
+    document.querySelectorAll("[data-nav-toggler]");
+const /** {NodeElement} */ $navbar =
+    document.querySelector("[data-navigation]");
+const /** {NodeElement} */ $scrim = document.querySelector("[data-scrim]");
+
+addEventOnElements($navTogglers, "click", function () {
+  $navbar.classList.toggle("show");
+  $scrim.classList.toggle("active");
+});
 
 /**
  * Filter functionality
